27b1f50ccf72ce254f177615971eb44ff4549a04
jyurek <jyurek@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2008-01-02 17:28:59 +0000

    attempts at getting tests for the notifier
    git-svn-id: https://svn.thoughtbot.com/plugins/hoptoad_notifier/trunk@276 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- lib/hoptoad_notifier.rb
+++ lib/hoptoad_notifier.rb
@@ -10,108 +10,102 @@ module HoptoadNotifier
   def self.filter_params; @filter_params ||= []; end
   
   module Catcher
-    def self.included target
-      target.send( :include, Handlers )
+     
+    private
+    
+    def rescue_action_in_public exception
+      if is_a_404?(exception)
+        render_not_found_page
+      else
+        render_error_page
+        data = {
+          'project_name'  => HoptoadNotifier.app_name,
+          'error_message' => exception.message,
+          'backtrace' => clean_backtrace(exception.backtrace).to_json,
+          'request'   => {
+            'params'     => clean_params(request.parameters.to_hash),
+            'rails_root' => File.expand_path(RAILS_ROOT),
+            'url'        => "#{request.protocol}#{request.host}#{request.request_uri}"
+          }.to_json,
+          'session' => {
+            'key' => session.instance_variable_get("@session_id"),
+            'data' => session.instance_variable_get("@data")
+          }.to_json,
+          'environment' => ENV.to_hash.to_json
+        }
+        inform_hoptoad(data)
+      end
+    end
+
+    def render_not_found_page
+      respond_to do |wants|
+        wants.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => :not_found }
+        wants.all  { render :nothing => true, :status => :not_found }
+      end
+    end
+
+    def render_error_page
+      respond_to do |wants|
+        wants.html { render :file => "#{RAILS_ROOT}/public/500.html", :status => :internal_server_error }
+        wants.all  { render :nothing => true, :status => :internal_server_error }
+      end
+    end
+
+    def inform_hoptoad data
+      url = HoptoadNotifier.url
+      Net::HTTP.start(url.host, url.port) do |http|
+        response = http.post(url.path, to_params(data), {'Accept', 'text/xml, application/xml'})
+        case response
+        when Net::HTTPSuccess then
+          logger.info "Hoptoad Success: #{response.class}"
+        when Net::HTTPRedirection then
+          logger.info "Hoptoad Success: #{response.class}"
+        else
+          logger.error "Hoptoad Failure: #{response.class}"
+        end
+      end
     end
     
-    module Handlers
-      
-      private
-      
-      def rescue_action_in_public exception
-        if is_a_404?(exception)
-          render_not_found_page
-        else
-          render_error_page
-          data = {
-            'project_name'  => HoptoadNotifier.app_name,
-            'error_message' => exception.message,
-            'backtrace' => clean_backtrace(exception.backtrace).to_json,
-            'request'   => {
-              'params'     => clean_params(request.parameters.to_hash),
-              'rails_root' => File.expand_path(RAILS_ROOT),
-              'url'        => "#{request.protocol}#{request.host}#{request.request_uri}"
-            }.to_json,
-            'session' => {
-              'key' => session.instance_variable_get("@session_id"),
-              'data' => session.instance_variable_get("@data")
-            }.to_json,
-            'environment' => ENV.to_hash.to_json
-          }
-          inform_hoptoad(data)
-        end
-      end
-
-      def render_not_found_page
-        respond_to do |wants|
-          wants.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => :not_found }
-          wants.all  { render :nothing => true, :status => :not_found }
-        end
-      end
-
-      def render_error_page
-        respond_to do |wants|
-          wants.html { render :file => "#{RAILS_ROOT}/public/500.html", :status => :internal_server_error }
-          wants.all  { render :nothing => true, :status => :internal_server_error }
-        end
-      end
-
-      def inform_hoptoad data
-        url = HoptoadNotifier.url
-        Net::HTTP.start(url.host, url.port) do |http|
-          response = http.post(url.path, to_params(data), {'Accept', 'text/xml, application/xml'})
-          case response
-          when Net::HTTPSuccess then
-            logger.info "Hoptoad Success: #{response.class}"
-          when Net::HTTPRedirection then
-            logger.info "Hoptoad Success: #{response.class}"
-          else
-            logger.error "Hoptoad Failure: #{response.class}"
-          end
-        end
-      end
-      
-      def is_a_404? exception
-        [ 
-          ActiveRecord::RecordNotFound,
-          ActionController::UnknownController,
-          ActionController::UnknownAction
-        ].include?( exception )
-      end
-      
-      def to_params thing, context = "notice"
-        case thing
-        when Hash
-          thing.map{|key, val| to_params(val, "#{context}[#{key}]") }.join("&")
-        when Array
-          thing.map{|val| to_params(val, "#{context}[]") }.join("&")
-        else
-          "#{CGI.escape(context)}=#{CGI.escape(thing.to_s)}"
-        end
-      end
-      
-      def clean_backtrace backtrace
-        backtrace.to_a.map do |line|
-          line = line.to_s
-          line.gsub!(/#{RAILS_ROOT}/, "[RAILS_ROOT]")
-          Gem.path.each do |path|
-            line.gsub!(/#{path}/, "[GEM_PATH]")
-          end
-          line.gsub!(/^\.\//, "")
-          line
-        end
-      end
-      
-      def clean_params params
-        params.each do |k, v|
-          should_filter ||= k.to_s.match(/password/)
-          HoptoadNotifier.filter_params.each do |filter|
-            should_filter ||= k.to_s.match(/#{filter}/)
-          end
-          params[k] = "<filtered>" if should_filter
-        end
-      end
-      
+    def is_a_404? exception
+      [ 
+        ActiveRecord::RecordNotFound,
+        ActionController::UnknownController,
+        ActionController::UnknownAction
+      ].include?( exception )
     end
+    
+    def to_params thing, context = "notice"
+      case thing
+      when Hash
+        thing.map{|key, val| to_params(val, "#{context}[#{key}]") }.join("&")
+      when Array
+        thing.map{|val| to_params(val, "#{context}[]") }.join("&")
+      else
+        "#{CGI.escape(context)}=#{CGI.escape(thing.to_s)}"
+      end
+    end
+    
+    def clean_backtrace backtrace
+      backtrace.to_a.map do |line|
+        line = line.to_s
+        line.gsub!(/#{RAILS_ROOT}/, "[RAILS_ROOT]")
+        Gem.path.each do |path|
+          line.gsub!(/#{path}/, "[GEM_PATH]")
+        end
+        line.gsub!(/^\.\//, "")
+        line
+      end
+    end
+    
+    def clean_params params
+      params.each do |k, v|
+        should_filter ||= k.to_s.match(/password/)
+        HoptoadNotifier.filter_params.each do |filter|
+          should_filter ||= k.to_s.match(/#{filter}/)
+        end
+        params[k] = "<filtered>" if should_filter
+      end
+    end
+      
   end
 end
--- test/hoptoad_notifier_test.rb
+++ test/hoptoad_notifier_test.rb
@@ -1,5 +1,84 @@
 require 'test/unit'
+require 'rubygems'
+require 'ruby-debug'
+require 'ruby2ruby'
+require 'mocha'
+require 'shoulda'
+require 'action_controller'
+require 'action_controller/test_process'
+require File.join(File.dirname(__FILE__), "..", "lib", "hoptoad_notifier")
+
+class HoptoadController < ActionController::Base
+  def rescue_action e
+    puts "rescue_action"
+    rescue_action_in_public e
+  end
+  
+  def rescue_action_in_public e
+    puts "rescue_action_in_public"
+    raise e
+  end
+  
+  def do_raise
+    render :text => "raise"
+    raise "Hoptoad"
+  end
+  
+  def do_not_raise
+    render :text => "no raise"
+  end
+end
 
 class HoptoadNotifierTest < Test::Unit::TestCase
+  def request action = nil, method = :get
+    @request = ActionController::TestRequest.new({
+      "controller" => "hoptoad",
+      "action"     => action ? action.to_s : "",
+      "_method"    => method.to_s
+    })
+    @response = ActionController::TestResponse.new
+    @controller.process(@request, @response)
+  end
+
+  context "The hoptoad controller" do
+    setup do
+      @controller = ::HoptoadController.new
+    end
+
+    context "with no notifier catcher" do
+      should "not prevent raises" do
+        assert_raises RuntimeError do
+          request("do_raise")
+        end
+      end
+
+      should "allow a non-raising action to complete" do
+        assert_nothing_raised do
+          request("do_not_raise")
+        end
+      end
+    end
+    
+    context "with the notifier installed" do
+      setup do
+        ::HoptoadController.send(:include, ::HoptoadNotifier::Catcher)
+        assert @controller.private_methods.include?("inform_hoptoad")
+        debugger
+      end
+      
+      should "prevent raises" do
+        @controller.expects(:inform_hoptoad)
+        assert_nothing_raised do
+          request("do_raise")
+        end
+      end
+
+      should "allow a non-raising action to complete" do
+        assert_nothing_raised do
+          request("do_not_raise")
+        end
+      end
+    end
+  end
 
 end
