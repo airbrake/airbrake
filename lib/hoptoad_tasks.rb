9cdd038b0e8f0bcd357590ddb3fb1e7e48eef601
Tammer Saleh <tsaleh@thoughtbot.com>
2009-03-23 09:40:17 -0700

    Bugfixes to deploy task

--- lib/hoptoad_tasks.rb
+++ lib/hoptoad_tasks.rb
@@ -10,12 +10,12 @@ module HoptoadTasks
     end
 
     if rails_env.blank?
-      puts "I don't know to which Rails environment you are deploying (use the TO='production' option)."
+      puts "I don't know to which Rails environment you are deploying (use the TO=production option)."
       return false
     end
 
-    url = URI.parse("http://hoptoadapp.com/deploys")
-    response = Net::HTTP.post_form(url, :api_key => HoptoadNotifier.api_key, :deploy => {:rails_env => rails_env})
+    url = URI.parse("http://#{HoptoadNotifier.host}/deploys")
+    response = Net::HTTP.post_form(url, :api_key => HoptoadNotifier.api_key, "deploy[rails_env]" => rails_env)
     puts response.body
     return Net::HTTPSuccess === response
   end
--- test/hoptoad_tasks_test.rb
+++ test/hoptoad_tasks_test.rb
@@ -51,10 +51,9 @@ class HoptoadTasksTest < ActiveSupport::TestCase
                     returns(successful_response)
         end
 
-        before_should "use valid deploy params" do
-          deploy_params = {:rails_env => "staging"}
+        before_should "use send the rails_env param" do
           Net::HTTP.expects(:post_form).
-                    with(kind_of(URI), has_entries(:deploy => deploy_params)).
+                    with(kind_of(URI), has_entries("deploy[rails_env]" => "staging")).
                     returns(successful_response)
         end
 
@@ -78,6 +77,24 @@ class HoptoadTasksTest < ActiveSupport::TestCase
           Net::HTTP.expects(:post_form).with(any_parameters).returns(successful_response('body'))
         } do
           assert @output
+        end
+      end
+    end
+
+    context "in a configured project with custom host" do
+      setup do
+        HoptoadNotifier.configure do |config| 
+          config.api_key = "1234123412341234"
+          config.host = "custom.host"
+        end
+      end
+
+      context "on deploy_to('staging')" do
+        setup { @output = HoptoadTasks.deploy_to("staging") }
+
+        before_should "post to the custom host" do
+          URI.stubs(:parse).with('http://custom.host/deploys').returns(:uri)
+          Net::HTTP.expects(:post_form).with(:uri, kind_of(Hash)).returns(successful_response)
         end
       end
     end
