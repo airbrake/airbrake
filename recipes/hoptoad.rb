8285bd2ec7ae2184f8d1bb23e35058fb96c093ab
Chad Pytel <cpytel@thoughtbot.com>
2009-03-27 12:54:59 -0400

    remove elipse

--- recipes/hoptoad.rb
+++ recipes/hoptoad.rb
@@ -14,7 +14,7 @@ namespace :deploy do
     rails_env = fetch(:rails_env, "production")
     local_user = ENV['USER'] || ENV['USERNAME']
     notify_command = "rake RAILS_ENV=#{rails_env} hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
-    puts "Notifying Hoptoad of Deploy (#{notify_command})..."
+    puts "Notifying Hoptoad of Deploy (#{notify_command})"
     `#{notify_command}`
     puts "Hoptoad Notification Complete."
   end
