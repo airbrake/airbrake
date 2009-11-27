13f3be77ff4cec7f38bae95e0a40fc2a2b19a6cb
Jason Morrison <jmorrison@thoughtbot.com>
2009-11-27 12:49:18 -0500

    Bump hoptoad_notifier version to 2.0.6

--- lib/hoptoad_notifier.rb
+++ lib/hoptoad_notifier.rb
@@ -11,7 +11,7 @@ require 'hoptoad_notifier/backtrace'
 # Plugin for applications to automatically post errors to the Hoptoad of their choice.
 module HoptoadNotifier
 
-  VERSION = "2.0.5"
+  VERSION = "2.0.6"
   API_VERSION = "2.0"
   LOG_PREFIX = "** [Hoptoad] "
 
