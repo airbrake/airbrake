72dd780cc8071f15f22792f3d715437bb490cfd6
Joe Ferris <joe.r.ferris@gmail.com>
2010-01-27 15:13:04 -0500

    Added docs to HoptoadNotifier::Rack

--- lib/hoptoad_notifier/rack.rb
+++ lib/hoptoad_notifier/rack.rb
@@ -1,4 +1,22 @@
 module HoptoadNotifier
+  # Middleware for Rack applications. Any errors raised by the upstream
+  # application will be delivered to Hoptoad and re-raised.
+  #
+  # Synopsis:
+  #
+  #   require 'rack'
+  #   require 'hoptoad_notifier'
+  #
+  #   HoptoadNotifier.configure do |config|
+  #     config.api_key = 'my_api_key'
+  #   end
+  #
+  #   app = Rack::Builder.app do
+  #     use HoptoadNotifier::Rack
+  #     run lambda { |env| raise "Rack down" }
+  #   end
+  #
+  # Use a standard HoptoadNotifier.configure call to configure your api key.
   class Rack
     def initialize(app)
       @app = app
