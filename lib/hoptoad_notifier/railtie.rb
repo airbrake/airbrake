fb31464d4b147cc2cf8cea19eca2013f112c2116
Josh Kalderimis <josh.kalderimis@gmail.com>
2010-05-26 14:10:33 +0200

    railtie_name is deprecated

--- lib/hoptoad_notifier/railtie.rb
+++ lib/hoptoad_notifier/railtie.rb
@@ -3,8 +3,6 @@ require 'rails'
 
 module HoptoadNotifier
   class Railtie < Rails::Railtie
-    railtie_name :hoptoad_notifier
-
     rake_tasks do
       require "hoptoad_notifier/rails3_tasks"
     end
