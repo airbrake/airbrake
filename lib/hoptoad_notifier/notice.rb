ac1c75af0c807edd32bdf6a227e6db57dfd5331f
Jason Morrison <jmorrison@thoughtbot.com>
2010-07-01 18:04:05 -0400

    Use the Rails 3 params filters when available

--- lib/hoptoad_notifier/notice.rb
+++ lib/hoptoad_notifier/notice.rb
@@ -95,6 +95,7 @@ module HoptoadNotifier
         "#{exception.class.name}: #{exception.message}"
       end
 
+      also_use_rack_params_filters
       find_session_data
       clean_params
     end
@@ -315,5 +316,12 @@ module HoptoadNotifier
     def rack_session
       args[:rack_env]['rack.session'] if args[:rack_env]
     end
+
+    def also_use_rack_params_filters
+      if args[:rack_env]
+        self.params_filters += rack_request.env["action_dispatch.parameter_filter"]
+      end
+    end
+
   end
 end
