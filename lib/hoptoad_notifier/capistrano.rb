def715cf37aa92faaf8301284e603ccc0f64269a
Tom Meier <ozmeier@yahoo.co.uk>
2010-12-03 10:18:31 +1100

    Use capistrano rake variable so commands can be prefixed, such as bundle exec

--- lib/hoptoad_notifier/capistrano.rb
+++ lib/hoptoad_notifier/capistrano.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :notify_hoptoad, :except => { :no_release => true } do
       rails_env = fetch(:hoptoad_env, fetch(:rails_env, "production"))
       local_user = ENV['USER'] || ENV['USERNAME']
-      executable = RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'
+      executable = RUBY_PLATFORM.downcase.include?('mswin') ? fetch(:rake, 'rake.bat') : fetch(:rake, 'rake')
       notify_command = "#{executable} hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
       notify_command << " DRY_RUN=true" if dry_run
       notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
