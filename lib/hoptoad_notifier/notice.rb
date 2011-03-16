9f153b33e6f0e44d73535d297f61f4db96a8436c
Jon Yurek <jyurek@thoughtbot.com>
2011-03-16 16:46:41 -0400

    Prevent recursive data structures

--- lib/hoptoad_notifier/notice.rb
+++ lib/hoptoad_notifier/notice.rb
@@ -226,14 +226,16 @@ module HoptoadNotifier
     # Removes non-serializable data. Allowed data types are strings, arrays,
     # and hashes. All other types are converted to strings.
     # TODO: move this onto Hash
-    def clean_unserializable_data(data)
+    def clean_unserializable_data(data, stack = [])
+      return "[possible infinite recursion halted]" if stack.any?{|item| item == data.object_id }
+
       if data.respond_to?(:to_hash)
         data.to_hash.inject({}) do |result, (key, value)|
-          result.merge(key => clean_unserializable_data(value))
+          result.merge(key => clean_unserializable_data(value, stack + [data.object_id]))
         end
       elsif data.respond_to?(:to_ary)
         data.collect do |value|
-          clean_unserializable_data(value)
+          clean_unserializable_data(value, stack + [data.object_id])
         end
       else
         data.to_s
