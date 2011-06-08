d47c899474ad7d2e9e6666531e30152d37067f5d
Jon Yurek <jyurek@thoughtbot.com>
2011-06-08 13:40:10 -0400

    A Gemfile for unit tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,14 +1,6 @@
 require 'test/unit'
 require 'rubygems'
 
-gem "activesupport", "= 2.3.8"
-gem "activerecord",  "= 2.3.8"
-gem "actionpack",    "= 2.3.8"
-gem "nokogiri",      "= 1.4.3.1"
-gem "shoulda",       "= 2.11.3"
-gem 'bourne', '>= 1.0'
-gem "sham_rack",     "~> 1.3.0"
-
 $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require 'thread'
