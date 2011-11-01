f12aa5355292a3ac8efbba2673c800e5aff067b3
Jonathan Siegel <jonathan@Jonathans-11-MBA.local>
2011-11-01 11:19:19 +0800

    Now running airbrake deploy notifications on the REMOTE machine.

--- lib/airbrake/capistrano.rb
+++ lib/airbrake/capistrano.rb
@@ -9,17 +9,25 @@ module Airbrake
         after "deploy:migrations", "airbrake:deploy"
 
         namespace :airbrake do
-          desc "Notify Airbrake of the deployment"
+          desc <<-DESC
+            Notify Airbrake of the deployment by running the notification on the REMOTE machine.
+              - Run remotely so we use remote API keys, environment, etc.
+          DESC
           task :deploy, :except => { :no_release => true } do
             rails_env = fetch(:airbrake_env, fetch(:rails_env, "production"))
             local_user = ENV['USER'] || ENV['USERNAME']
             executable = RUBY_PLATFORM.downcase.include?('mswin') ? fetch(:rake, 'rake.bat') : fetch(:rake, 'rake')
-            notify_command = "#{executable} airbrake:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+            directory = current_release
+            notify_command = "cd #{directory}; #{executable} RAILS_ENV=#{rails_env} airbrake:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
             notify_command << " DRY_RUN=true" if dry_run
             notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
             logger.info "Notifying Airbrake of Deploy (#{notify_command})"
-            `#{notify_command}` if !configuration.dry_run
-            logger.info "Airbrake Notification Complete."
+            if configuration.dry_run
+              logger.info "DRY RUN: Notification not actually run."
+            else
+              run(notify_command) 
+              logger.info "Airbrake Notification Complete."
+            end
           end
         end
       end
