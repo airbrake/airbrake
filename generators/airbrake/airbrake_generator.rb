30e3cb4d51301b87c94554705818197f360effc1
Jonathan Siegel <usiegj00@gmail.com>
2011-12-28 21:08:12 -0800

    Added beta support fer cedar.

--- generators/airbrake/airbrake_generator.rb
+++ generators/airbrake/airbrake_generator.rb
@@ -62,7 +62,9 @@ class AirbrakeGenerator < Rails::Generator::Base
 
   def heroku_api_key
     app = options[:app] ? " --app #{options[:app]}" : ''
-    `heroku console#{app} 'puts ENV[%{HOPTOAD_API_KEY}]'`.split("\n").first
+    cedar = !`heroku stack #{app} | grep \*\ cedar`.blank?
+    run = " run " unless cedar
+    `heroku #{cedar} console#{app} 'puts ENV[%{HOPTOAD_API_KEY}]'`.split("\n").first
   end
 
   def heroku?
