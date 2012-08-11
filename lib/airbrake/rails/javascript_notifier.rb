39a72d5a6748e1093a27e56239cbfd1abb1e8d4e
Subhash Chandra <subhash@investopresto.com>
2012-08-11 18:09:04 +0530

    split javascript notifier into loader and configuration

--- lib/airbrake/rails/javascript_notifier.rb
+++ lib/airbrake/rails/javascript_notifier.rb
@@ -3,30 +3,48 @@ module Airbrake
     module JavascriptNotifier
       def self.included(base) #:nodoc:
         base.send :helper_method, :airbrake_javascript_notifier
+        base.send :helper_method, :airbrake_javascript_loader
+        base.send :helper_method, :airbrake_javascript_configuration
       end
 
       private
 
       def airbrake_javascript_notifier
+        airbrake_javascript_loader + airbrake_javascript_configuration
+      end
+
+      def airbrake_javascript_loader
         return unless Airbrake.configuration.public?
 
-        path = File.join File.dirname(__FILE__), '..', '..', 'templates', 'javascript_notifier.erb'
-        host = Airbrake.configuration.host.dup
-        port = Airbrake.configuration.port
-        host << ":#{port}" unless [80, 443].include?(port)
+        path = File.join File.dirname(__FILE__), '..', '..', 'templates', 'javascript_notifier_loader.erb'
 
+        render_part path, :host => host
+      end
+
+      def airbrake_javascript_configuration
+        return unless Airbrake.configuration.public?
+
+        path = File.join File.dirname(__FILE__), '..', '..', 'templates', 'javascript_notifier_configuration.erb'
+
+        options              = {
+          :host            => host,
+          :api_key         => Airbrake.configuration.js_api_key,
+          :environment     => Airbrake.configuration.environment_name,
+          :action_name     => action_name,
+          :controller_name => controller_name,
+          :url             => request.url
+        }
+
+        render_part path, options
+      end
+
+    protected
+      def render_part(path, locals={})
         options              = {
           :file              => path,
           :layout            => false,
           :use_full_path     => false,
-          :locals            => {
-            :host            => host,
-            :api_key         => Airbrake.configuration.js_api_key,
-            :environment     => Airbrake.configuration.environment_name,
-            :action_name     => action_name,
-            :controller_name => controller_name,
-            :url             => request.url
-          }
+          :locals            => locals
         }
 
         res = if @template
@@ -43,6 +61,11 @@ module Airbrake
 
       end
 
+      def host
+        host = Airbrake.configuration.host.dup
+        port = Airbrake.configuration.port
+        host << ":#{port}" unless [80, 443].include?(port)
+      end
     end
   end
 end
