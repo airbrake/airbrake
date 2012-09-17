11e73f6b311a9491562562c4b63b7695e6b0aabc
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-09-17 09:47:09 -0400

    Precompile our own js assets

--- lib/airbrake/engine.rb
+++ lib/airbrake/engine.rb
@@ -2,5 +2,11 @@ require 'rails'
 
 module Airbrake
   class Engine < ::Rails::Engine
+
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile << "airbrake-notifier.js"
+      end
+    end
   end
 end
