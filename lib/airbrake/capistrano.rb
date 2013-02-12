2c1a3a73c4f6f2f35f3887225559f709ece772b9
Bartosz Knapik <bartosz.knapik@llp.pl>
2013-02-12 17:16:31 +0100

    replace current_release with release_path

--- lib/airbrake/capistrano.rb
+++ lib/airbrake/capistrano.rb
@@ -19,7 +19,7 @@ module Airbrake
             airbrake_env = fetch(:airbrake_env, fetch(:rails_env, "production"))
             local_user = ENV['USER'] || ENV['USERNAME']
             executable = RUBY_PLATFORM.downcase.include?('mswin') ? fetch(:rake, 'rake.bat') : fetch(:rake, 'rake')
-            directory = configuration.current_release
+            directory = configuration.release_path
             notify_command = "cd #{directory}; #{executable} RAILS_ENV=#{rails_env} airbrake:deploy TO=#{airbrake_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
             notify_command << " DRY_RUN=true" if dry_run
             notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
