3bb93f34177d1a5a942e56b86bad938d447bc3d1
Arvind Kunday <arvind.kunday@rea-group.com>
2013-04-22 21:52:13 +1000

    Removing the .erb template handler

--- lib/airbrake/rails/javascript_notifier.rb
+++ lib/airbrake/rails/javascript_notifier.rb
@@ -8,7 +8,7 @@ module Airbrake
       private
 
       def airbrake_javascript_notifier_options
-        path = File.join File.dirname(__FILE__), '..', '..', 'templates', 'javascript_notifier.erb'
+        path = File.join File.dirname(__FILE__), '..', '..', 'templates', 'javascript_notifier'
         host = Airbrake.configuration.host.dup
         port = Airbrake.configuration.port
         host << ":#{port}" unless [80, 443].include?(port)
@@ -17,6 +17,7 @@ module Airbrake
           :file              => path,
           :layout            => false,
           :use_full_path     => false,
+          :handlers          => [:erb],
           :locals            => {
             :host            => host,
             :api_key         => Airbrake.configuration.js_api_key,
