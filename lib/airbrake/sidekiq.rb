1caf061178c3ab94da20d7d4359cbaf63d50dec9
shifi <marko.siftar@gmail.com>
2015-05-22 11:26:18 -0500

    fix syntax error in Sidekiq configuration

--- lib/airbrake/sidekiq.rb
+++ lib/airbrake/sidekiq.rb
@@ -2,7 +2,7 @@ gem 'sidekiq', '~> 3.0'
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.error_handlers << ->(exception, context)
+  config.error_handlers << lambda do |exception, context|
     Airbrake.notify_or_ignore(exception, :parameters => context)
   end
 end
