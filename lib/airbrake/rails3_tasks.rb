a5f3820be5786d1386b6ba92fafc5218d22706f5
shifi <marko.siftar@gmail.com>
2015-06-02 21:46:50 -0500

    override BetterErrors middleware when using airbrake test rake task

--- lib/airbrake/rails3_tasks.rb
+++ lib/airbrake/rails3_tasks.rb
@@ -59,6 +59,11 @@ namespace :airbrake do
     class ActionDispatch::DebugExceptions; def call(env); @app.call(env); end; end
     class ActionDispatch::ShowExceptions; def call(env); @app.call(env); end; end
 
+    # Override BetterErrors middleware if present
+    if defined?(BetterErrors::Middleware)
+      class BetterErrors::Middleware; def call(env); @app.call(env); end; end
+    end
+
     require './app/controllers/application_controller'
 
     class AirbrakeTestingException < RuntimeError; end
