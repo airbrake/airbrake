17f2f0b6050b8ec2e9c6517142537d4b19fafef0
Mark Burns <markthedeveloper@gmail.com>
2015-11-16 16:44:44 +0900

    Extract class refactor Airbrake::Notice::JsonBuilder

--- lib/airbrake/notice.rb
+++ lib/airbrake/notice.rb
@@ -1,6 +1,6 @@
 require 'socket'
-require 'multi_json'
 require 'airbrake/notice/xml_builder'
+require 'airbrake/notice/json_builder'
 
 module Airbrake
   class Notice
@@ -130,44 +130,7 @@ module Airbrake
     end
 
     def to_json
-      MultiJson.dump({
-        'notifier' => {
-          'name'    => 'airbrake',
-          'version' => Airbrake::VERSION,
-          'url'     => 'https://github.com/airbrake/airbrake'
-          },
-        'errors' => [{
-            'type'       => error_class,
-            'message'    => error_message,
-            'backtrace'  => backtrace.lines.map do |line|
-                {
-                  'file'     => line.file,
-                  'line'     => line.number.to_i,
-                  'function' => line.method_name
-                }
-            end
-          }],
-         'context' => {}.tap do |hash|
-            if request_present?
-              hash['url']           = url
-              hash['component']     = controller
-              hash['action']        = action
-              hash['rootDirectory'] = File.dirname(project_root)
-              hash['environment']   = environment_name
-            end
-           end.tap do |hash|
-            next if user.empty?
-
-            hash['userId']    = user[:id]
-            hash['userName']  = user[:name]
-            hash['userEmail'] = user[:email]
-          end
-
-      }.tap do |hash|
-          hash['environment'] = cgi_data     unless cgi_data.empty?
-          hash['params']      = parameters   unless parameters.empty?
-          hash['session']     = session_data unless session_data.empty?
-      end)
+      Notice::JsonBuilder.render(self)
     end
 
     # Determines if this notice should be ignored
--- lib/airbrake/notice/xml_builder.rb
+++ lib/airbrake/notice/xml_builder.rb
@@ -1,46 +1,9 @@
 require 'builder'
+require 'airbrake/notice/base_builder'
 
 module Airbrake
   class Notice
-    class XmlBuilder
-      def self.render(notice)
-        new(notice).render
-      end
-
-      attr_reader :notice
-
-      ATTRIBUTES_TO_RENDER = %w(
-        backtrace
-        notifier_name
-        notifier_version
-        notifier_url
-        api_key
-        error_message
-        error_class
-        request_present?
-        url
-        controller
-        action
-        parameters
-        session_data
-        cgi_data
-        project_root
-        environment_name
-        hostname
-        user
-        framework
-      )
-
-      ATTRIBUTES_TO_RENDER.each do |attr|
-        define_method attr do
-          notice.send(attr)
-        end
-      end
-
-      def initialize(notice)
-        @notice = notice
-      end
-
+    class XmlBuilder < BaseBuilder
       def render
         builder = Builder::XmlMarkup.new
         builder.instruct!
