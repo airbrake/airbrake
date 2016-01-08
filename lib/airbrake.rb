a2c476cc198fee6679092b92ebef25c9e5e881c8
Kyrylo Silin <silin@kyrylo.org>
2016-01-08 15:38:17 +0200

    airbrake: load Rake integration only if Rake::Task is defined
    Fixes #449 (Airbrake 5 Breaks Debugging)
    
    The problem occurs *only* in RubyMine. For some reason when running
    tests with help of it (via the GUI) the `Rake` constant is defined,
    but none of the Rake library is loaded. I tried to debug this for a
    while, but couldn't find any clues.
    
    The fix is to rely on other constant being present - `Rake::Task`.

--- lib/airbrake.rb
+++ lib/airbrake.rb
@@ -16,7 +16,7 @@ if defined?(Rack)
   require 'airbrake/rails/railtie' if defined?(Rails)
 end
 
-require 'airbrake/rake/task_ext' if defined?(Rake)
+require 'airbrake/rake/task_ext' if defined?(Rake::Task)
 require 'airbrake/resque/failure' if defined?(Resque)
 require 'airbrake/sidekiq/error_handler' if defined?(Sidekiq)
 require 'airbrake/delayed_job/plugin' if defined?(Delayed)
