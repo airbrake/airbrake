014006473d8511d7fef4b40eb1595b6f02259192
Joe Marty <jmarty@iexposure.com>
2016-10-21 16:25:28 -0500

    Added more tests, made utility method private

--- spec/unit/rack/user_spec.rb
+++ spec/unit/rack/user_spec.rb
@@ -73,19 +73,67 @@ RSpec.describe Airbrake::Rack::User do
         end
 
         context "but it requires parameters" do
-          before do
-            class DummyController
-              def current_user(_)
-                "username"
+          let(:controller) { dummy_controller.new }
+          subject { described_class.extract(env) }
+
+          context ": current_user(a)" do
+            let(:dummy_controller) do
+              Class.new do
+                def current_user(_a)
+                  "username"
+                end
               end
             end
-          end
-          let(:controller) { DummyController.new }
-          after { Object.send(:remove_const, :DummyController) }
 
-          it "returns nil" do
-            retval = described_class.extract(env)
-            expect(retval).to be_nil
+            it { should be_nil }
+          end
+
+          context ": current_user(a, b)" do
+            let(:dummy_controller) do
+              Class.new do
+                def current_user(_a, _b)
+                  "username"
+                end
+              end
+            end
+
+            it { should be_nil }
+          end
+
+          context ": current_user(a, *b)" do
+            let(:dummy_controller) do
+              Class.new do
+                def current_user(_a, *_b)
+                  "username"
+                end
+              end
+            end
+
+            it { should be_nil }
+          end
+
+          context ": current_user(a, b, *c, &d)" do
+            let(:dummy_controller) do
+              Class.new do
+                def current_user(_a, _b, *_c, &_d)
+                  "username"
+                end
+              end
+            end
+
+            it { should be_nil }
+          end
+
+          context ": current_user(*a)" do
+            let(:dummy_controller) do
+              Class.new do
+                def current_user(*_a)
+                  "username"
+                end
+              end
+            end
+
+            it { should be_a(described_class) }
           end
         end
       end
