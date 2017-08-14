66ca5811421d043c6d90e679bc27813e00a1462b
Kyrylo Silin <silin@kyrylo.org>
2017-08-15 03:50:33 +0800

    capistrano/capistrano2: simplify integration
    With these changes we don't need to ignore any Rubocop offences.

--- lib/airbrake/capistrano/capistrano2.rb
+++ lib/airbrake/capistrano/capistrano2.rb
@@ -2,7 +2,6 @@ module Airbrake
   ##
   # The Capistrano v2 integration.
   module Capistrano
-    # rubocop:disable Metrics/AbcSize
     def self.load_into(config)
       config.load do
         after 'deploy',            'airbrake:deploy'
@@ -12,28 +11,31 @@ module Airbrake
         namespace :airbrake do
           desc "Notify Airbrake of the deploy"
           task :deploy, except: { no_release: true }, on_error: :continue do
-            username = Shellwords.shellescape(ENV['USER'] || ENV['USERNAME'])
-            command = <<-CMD
-              cd #{config.release_path} && \
+            run(
+              <<-CMD, once: true
+                cd #{config.release_path} && \
 
-              RACK_ENV=#{fetch(:rack_env, nil)} \
-              RAILS_ENV=#{fetch(:rails_env, nil)} \
+                RACK_ENV=#{fetch(:rack_env, nil)} \
+                RAILS_ENV=#{fetch(:rails_env, nil)} \
 
-              bundle exec rake airbrake:deploy \
-                USERNAME=#{username} \
-                ENVIRONMENT=#{fetch(:rails_env, 'production')} \
-                REVISION=#{current_revision.strip} \
-                REPOSITORY=#{repository} \
-                VERSION=#{fetch(:app_version, nil)}
-            CMD
-
-            run(command, once: true)
+                bundle exec rake airbrake:deploy \
+                  USERNAME=#{username} \
+                  ENVIRONMENT=#{fetch(:rails_env, 'production')} \
+                  REVISION=#{current_revision.strip} \
+                  REPOSITORY=#{repository} \
+                  VERSION=#{fetch(:app_version, nil)}
+              CMD
+            )
             logger.info 'Notified Airbrake of the deploy'
           end
         end
       end
     end
-    # rubocop:enable Metrics/AbcSize
+
+    def self.username
+      Shellwords.shellescape(ENV['USER'] || ENV['USERNAME'])
+    end
+    private_class_method :username
   end
 end
 
