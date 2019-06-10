ac083adffb4669ccd4f53e7178b176f6389fe5e4
Paul Gideon Dann <pdgiddie@gmail.com>
2019-06-10 15:44:24 +0100

    Add specs for new max_retries behaviour in RetryableJobsFilter

--- spec/unit/sidekiq/retryable_jobs_filter_spec.rb
+++ spec/unit/sidekiq/retryable_jobs_filter_spec.rb
@@ -38,5 +38,27 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.2.2')
       notice = build_notice('retry' => 3)
       expect { filter.call(notice) }.to_not raise_error
     end
+
+    context 'with max_retries = 2' do
+      subject(:filter) { described_class.new(max_retries: 2) }
+
+      it "ignores notices when retry_count is null" do
+        notice = build_notice('retry' => 5)
+        filter.call(notice)
+        expect(notice).to be_ignored
+      end
+
+      it "ignores notices when retry_count is 0" do
+        notice = build_notice('retry' => 5, 'retry_count' => 0)
+        filter.call(notice)
+        expect(notice).to be_ignored
+      end
+
+      it "does not ignore notices when retry_count is 1" do
+        notice = build_notice('retry' => 5, 'retry_count' => 1)
+        filter.call(notice)
+        expect(notice).to_not be_ignored
+      end
+    end
   end
 end
