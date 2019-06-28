91e24e60d572b645ff3a8e13f1db86bf27ac7ce4
Paul Gideon Dann <pdgiddie@gmail.com>
2019-06-28 12:26:02 +0100

    Add option to notify of sidekiq retries after a given number (#979)
    * Add option to notify of sidekiq retries after a given number
    
    This is useful if we don't want to be spammed by a couple of retries,
    but *do* want repeated warnings if a job continues to fail multiple
    times.
    
    * Fix accidental uses of build_notice() in place of pre-built notice

--- spec/unit/sidekiq/retryable_jobs_filter_spec.rb
+++ spec/unit/sidekiq/retryable_jobs_filter_spec.rb
@@ -3,8 +3,9 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.2.2')
   require 'sidekiq/cli'
   require 'airbrake/sidekiq'
 
-  RSpec.describe "airbrake/sidekiq/retryable_jobs_filter" do
-    subject(:filter) { Airbrake::Sidekiq::RetryableJobsFilter.new }
+  RSpec.describe Airbrake::Sidekiq::RetryableJobsFilter do
+    subject(:filter) { described_class.new }
+
     def build_notice(job = nil)
       Airbrake::Notice.new(StandardError.new, job: job)
     end
@@ -12,13 +13,13 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.2.2')
     it "does not ignore notices that are not from jobs" do
       notice = build_notice
       filter.call(notice)
-      expect(build_notice).to_not be_ignored
+      expect(notice).to_not be_ignored
     end
 
     it "does not ignore notices from jobs that have retries disabled" do
       notice = build_notice('retry' => false)
       filter.call(notice)
-      expect(build_notice).to_not be_ignored
+      expect(notice).to_not be_ignored
     end
 
     it "ignore notices from jobs that will be retried" do
@@ -36,6 +37,28 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.2.2')
     it "does not error if retry_count is missing" do
       notice = build_notice('retry' => 3)
       expect { filter.call(notice) }.to_not raise_error
+    end
+
+    context 'with max_retries = 2' do
+      subject(:filter) { described_class.new(max_retries: 2) }
+
+      it "ignores notices when retry_count is null" do
+        notice = build_notice('retry' => 5)
+        filter.call(notice)
+        expect(notice).to be_ignored
+      end
+
+      it "ignores notices when retry_count is 0" do
+        notice = build_notice('retry' => 5, 'retry_count' => 0)
+        filter.call(notice)
+        expect(notice).to be_ignored
+      end
+
+      it "does not ignore notices when retry_count is 1" do
+        notice = build_notice('retry' => 5, 'retry_count' => 1)
+        filter.call(notice)
+        expect(notice).to_not be_ignored
+      end
     end
   end
 end
