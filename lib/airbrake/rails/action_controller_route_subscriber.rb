c9eadea01893f2e41d89c75d532ea71af3b3e2e3
Kyrylo Silin <silin@kyrylo.org>
2019-11-27 14:55:33 +0800

    rails/app: return nil when request is unrecognized
    By returning nil explicitly we make sure that the code will not blow up in the
    future if Rails' `recognize` decides to return something other than a
    route (which happened to me locally on Rails 5 while I was testing something).
    
    Additionaly, we return our own Route now (again), so that we don't leak Rails
    code all over our codebase.

--- lib/airbrake/rails/action_controller_route_subscriber.rb
+++ lib/airbrake/rails/action_controller_route_subscriber.rb
@@ -18,19 +18,11 @@ module Airbrake
         )
         return unless route
 
-        routes[find_route_name(route)] = {
+        routes[route.path] = {
           method: event.method,
           response_type: event.response_type,
           groups: {}
         }
-      end
-
-      def find_route_name(route)
-        if route.app.respond_to?(:app) && route.app.app.respond_to?(:engine_name)
-          "#{route.app.app.engine_name}##{route.path.spec}"
-        else
-          route.path.spec.to_s
-        end
       end
     end
   end
--- lib/airbrake/rails/app.rb
+++ lib/airbrake/rails/app.rb
@@ -5,8 +5,19 @@ module Airbrake
     # @since v9.0.3
     # @api private
     class App
+      Route = Struct.new(:path)
+
+      # @param [] request
+      # @return [Airbrake::Rails::App::Route, nil]
       def self.recognize_route(request)
         ::Rails.application.routes.router.recognize(request) do |route, _params|
+          path =
+            if route.app.respond_to?(:app) && route.app.app.respond_to?(:engine_name)
+              "#{route.app.app.engine_name}##{route.path.spec}"
+            else
+              route.path.spec.to_s
+            end
+
           # Rails can recognize multiple routes for the given request. For
           # example, if we visit /users/2/edit, then Rails sees these routes:
           #   * "/users/:id/edit(.:format)"
@@ -14,8 +25,10 @@ module Airbrake
           #
           # We return the first route as, what it seems, the most optimal
           # approach.
-          return route
+          return Route.new(path)
         end
+
+        nil
       end
     end
   end
