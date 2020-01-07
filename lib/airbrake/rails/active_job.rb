8bff73b59f7c715d13a0108119c662cce65f005c
Kyrylo Silin <silin@kyrylo.org>
2020-01-07 14:56:19 +0800

    active_job: send execution statistics

--- lib/airbrake/rails/active_job.rb
+++ lib/airbrake/rails/active_job.rb
@@ -23,9 +23,32 @@ module Airbrake
         raise exception
       end
 
+      def self.perform(job, block)
+        timing = Airbrake::Benchmark.measure do
+          block.call
+        end
+      rescue StandardError => exception
+        Airbrake.notify_queue_sync(
+          queue: job.class.name,
+          error_count: 1,
+          timing: 0.01,
+        )
+        raise exception
+      else
+        Airbrake.notify_queue_sync(
+          queue: job.class.name,
+          error_count: 0,
+          timing: timing,
+        )
+      end
+
       included do
         rescue_from(Exception) do |exception|
           Airbrake::Rails::ActiveJob.notify_airbrake(exception, self)
+        end
+
+        around_perform do |job, block|
+          Airbrake::Rails::ActiveJob.perform(job, block)
         end
       end
     end
