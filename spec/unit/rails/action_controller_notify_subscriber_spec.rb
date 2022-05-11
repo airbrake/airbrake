e752fec3ba2d42fb187039ca17dc2b327989f551
Kyrylo Silin <silin@kyrylo.org>
2022-05-11 20:01:33 +0300

    spec/action_controller_notify_subscriber: use have_received
    This form is nicer because we run expectations after the main code, not before.

--- spec/unit/rails/action_controller_notify_subscriber_spec.rb
+++ spec/unit/rails/action_controller_notify_subscriber_spec.rb
@@ -10,12 +10,13 @@ RSpec.describe Airbrake::Rails::ActionControllerNotifySubscriber do
 
     before do
       allow(Airbrake::Rails::Event).to receive(:new).and_return(event)
+      allow(Airbrake).to receive(:notify_request)
     end
 
     context "when there are no routes in the request store" do
       it "doesn't notify requests" do
-        expect(Airbrake).not_to receive(:notify_request)
         subject.call([])
+        expect(Airbrake).not_to have_received(:notify_request)
       end
     end
 
@@ -38,8 +39,8 @@ RSpec.describe Airbrake::Rails::ActionControllerNotifySubscriber do
         end
 
         it "doesn't notify requests" do
-          expect(Airbrake).not_to receive(:notify_request)
           subject.call([])
+          expect(Airbrake).not_to have_received(:notify_request)
         end
       end
 
@@ -47,22 +48,27 @@ RSpec.describe Airbrake::Rails::ActionControllerNotifySubscriber do
         before do
           allow(Airbrake::Config.instance)
             .to receive(:performance_stats).and_return(true)
+
+          allow(event).to receive(:method).and_return('GET')
+          allow(event).to receive(:status_code).and_return(200)
+          allow(event).to receive(:time).and_return(Time.now)
+          allow(event).to receive(:duration).and_return(1.234)
         end
 
         it "sends request info to Airbrake" do
-          expect(Airbrake).to receive(:notify_request).with(
+          subject.call([])
+
+          expect(Airbrake).to have_received(:notify_request).with(
             hash_including(
               method: 'GET',
               route: '/test-route',
               status_code: 200,
             ),
           )
-          expect(event).to receive(:method).and_return('GET')
-          expect(event).to receive(:status_code).and_return(200)
-          expect(event).to receive(:time).and_return(Time.now)
-          expect(event).to receive(:duration).and_return(1.234)
-
-          subject.call([])
+          expect(event).to have_received(:method)
+          expect(event).to have_received(:status_code)
+          expect(event).to have_received(:time)
+          expect(event).to have_received(:duration)
         end
       end
     end
