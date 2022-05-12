6278acbc58b67e52d661b74c89b8f5c88ec00354
Kyrylo Silin <silin@kyrylo.org>
2022-05-12 19:05:29 +0300

    rails: factor out airbrake.active_record 'tie' code into a class

--- lib/airbrake/rails/railtie.rb
+++ lib/airbrake/rails/railtie.rb
@@ -5,8 +5,6 @@ module Airbrake
     # This railtie works for any Rails application that supports railties (Rails
     # 3.2+ apps). It makes Airbrake Ruby work with Rails and report errors
     # occurring in the application automatically.
-    #
-    # rubocop:disable Metrics/BlockLength
     class Railtie < ::Rails::Railtie
       initializer('airbrake.middleware') do |app|
         require 'airbrake/rails/railties/middleware_tie'
@@ -27,40 +25,8 @@ module Airbrake
       end
 
       initializer('airbrake.active_record') do
-        ActiveSupport.on_load(:active_record, run_once: true) do
-          # Reports exceptions occurring in some bugged ActiveRecord callbacks.
-          # Applicable only to the versions of Rails lower than 4.2.
-          if defined?(::Rails) &&
-             Gem::Version.new(::Rails.version) <= Gem::Version.new('4.2')
-            require 'airbrake/rails/active_record'
-            include Airbrake::Rails::ActiveRecord
-          end
-
-          if defined?(ActiveRecord)
-            # Send SQL queries.
-            require 'airbrake/rails/active_record_subscriber'
-            ActiveSupport::Notifications.subscribe(
-              'sql.active_record', Airbrake::Rails::ActiveRecordSubscriber.new
-            )
-
-            # Filter out parameters from SQL body.
-            if ::ActiveRecord::Base.respond_to?(:connection_db_config)
-              # Rails 6.1+ deprecates "connection_config" in favor of
-              # "connection_db_config", so we need an updated call.
-              Airbrake.add_performance_filter(
-                Airbrake::Filters::SqlFilter.new(
-                  ::ActiveRecord::Base.connection_db_config.configuration_hash[:adapter],
-                ),
-              )
-            else
-              Airbrake.add_performance_filter(
-                Airbrake::Filters::SqlFilter.new(
-                  ::ActiveRecord::Base.connection_config[:adapter],
-                ),
-              )
-            end
-          end
-        end
+        require 'airbrake/rails/railties/active_record_tie'
+        Railties::ActiveRecordTie.new.call
       end
 
       initializer('airbrake.active_job') do
@@ -84,6 +50,5 @@ module Airbrake
         end
       end
     end
-    # rubocop:enable Metrics/BlockLength
   end
 end
